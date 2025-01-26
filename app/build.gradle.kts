d08eea122ff6aa14035050c3626f89b4ed5a9c7e
MishaNikolaev <nmichail154@mail.ru>
2025-01-26 22:24:57 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,123 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.nmichail.moviesapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.nmichail.moviesapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.navigation.compose)
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.picasso)
+
+    // Lifecycle
+    implementation(libs.lifecycle.extensions)
+    implementation(libs.bundles.lifecycle)
+
+    // Coroutines
+    implementation(libs.bundles.coroutines)
+
+    // Coil
+    implementation(libs.coil)
+
+    //Accompanist
+    implementation(libs.bundles.accompanist)
+
+
+    //Media3
+    implementation(libs.media3.ui)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.bundles.firebase)
+
+    // ExoPlayer
+    api(libs.bundles.exoplayer)
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.session)
+
+    implementation (libs.hilt.android.v2511)
+    kapt (libs.dagger.hilt.compiler)
+
+    implementation (libs.gson)
+    implementation (libs.coil.compose.v260)
+
+    //data store
+    implementation (libs.androidx.datastore.preferences)
+
+    //Paging 3
+    implementation("androidx.paging:paging-runtime:3.3.5")
+    implementation("androidx.paging:paging-compose:3.3.5")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation ("androidx.room:room-paging:2.6.1")
+}
